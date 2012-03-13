9a9a48994212dc4068994ac5b796d68be3852a56
Artem Suschev <Suschev.Artem+github@gmail.com>
2012-03-14 01:24:39 +0200

    initial

---
+++ vk-async.js
@@ -0,0 +1,35 @@
+VK_async = new (function () {
+    functions = [];
+    api_script_path = "http://userapi.com/js/api/openapi.js?31";
+
+    this.ready = function (f){
+        if(typeof(VK) === 'undefined') {
+            functions.push(f);
+        } else {
+            f();
+        }
+    };
+
+    this.setAPIScriptPath = function(path) {
+        that.api_script_path = path;
+    }
+
+    this.init = function() {
+        jQuery(function() {
+            $.ajax({
+              url: api_script_path,
+              dataType: "script",
+              success: function() {
+                  for(var i=0;i<functions.length;i++) {
+                      functions[i].call(window);
+                  }
+              }
+            });
+          });
+    };
+
+    var init = this.init;
+    jQuery(function() {
+        init();
+    });
+})();
